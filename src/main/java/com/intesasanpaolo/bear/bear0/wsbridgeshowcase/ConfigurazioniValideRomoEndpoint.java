883a85f4c8b6fc1baf1137b1644de94360872695
Roberto Bruni <roberto.bruni@par-tec.it>
2019-12-18 15:13:42 +0100

    -  third commit

--- src/main/java/com/intesasanpaolo/bear/bear0/wsbridgeshowcase/ConfigurazioniValideRomoEndpoint.java
+++ src/main/java/com/intesasanpaolo/bear/bear0/wsbridgeshowcase/ConfigurazioniValideRomoEndpoint.java
@@ -1,10 +1,30 @@
 package com.intesasanpaolo.bear.bear0.wsbridgeshowcase;
 
+import com.intesasanpaolo.bear.bear0.model.FindConfigurazioniValideRomo;
+import com.intesasanpaolo.bear.bear0.model.FindConfigurazioniValideRomoResponse;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class ConfigurazioniValideRomoEndpoint {
 
-    private static final String NAMESPACE_URI = ""; //namespace of XSD schema
+    private static final String NAMESPACE_URI = "http://oracle.j2ee.ws/ejb/GecWebService"; //namespace of XSD schema
+
+    @Autowired
+    private ObjectFactory objectFactory;
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "findConfigurazioniValideRomo")
+    @ResponsePayload
+    public FindConfigurazioniValideRomoResponse findConfigurazioniValideRomo (@RequestPayload FindConfigurazioniValideRomo cfg){
+        FindConfigurazioniValideRomoResponse findConfigurazioniValideRomoResponse = null;
+        findConfigurazioniValideRomoResponse.setReturn(true);
+        return findConfigurazioniValideRomoResponse;
+
+    }
+
 
 }
