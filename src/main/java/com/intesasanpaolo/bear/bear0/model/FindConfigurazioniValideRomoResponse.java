23f0e5ac454c8ffb81e4d00a71c624b2e4e024bf
Roberto Bruni <roberto.bruni@par-tec.it>
2019-12-18 15:14:39 +0100

    fifth commit

---
+++ src/main/java/com/intesasanpaolo/bear/bear0/model/FindConfigurazioniValideRomoResponse.java
@@ -0,0 +1,63 @@
+//
+// Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.2.11 
+// Vedere <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
+// Generato il: 2019.12.18 alle 11:54:44 AM CET 
+//
+
+
+package com.intesasanpaolo.bear.bear0.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java per anonymous complex type.
+ * 
+ * <p>Il seguente frammento di schema specifica il contenuto previsto contenuto in questa classe.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="return" type="{http://www.w3.org/2001/XMLSchema}boolean"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "_return"
+})
+@XmlRootElement(name = "findConfigurazioniValideRomoResponse")
+public class FindConfigurazioniValideRomoResponse {
+
+    @XmlElement(name = "return")
+    protected boolean _return;
+
+    /**
+     * Recupera il valore della proprietà return.
+     * 
+     */
+    public boolean isReturn() {
+        return _return;
+    }
+
+    /**
+     * Imposta il valore della proprietà return.
+     * 
+     */
+    public void setReturn(boolean value) {
+        this._return = value;
+    }
+
+}
